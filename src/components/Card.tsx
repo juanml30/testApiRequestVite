f1ad4f9be48c7b4fbaa13eb3bd78048ca6e20f3a
Juan Moreno <moreno.l.juan@gmail.com>
2025-01-25 19:19:32 -0300

    feat(styles): add little style without framework

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,35 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { getWikipedia } from "../api/wikipedia";
 import { CardParams } from "../types";
 
-export const Card = ({holiday}: CardParams) => {
-    const [wikipedia, setWikipedia] = useState(null);
-    useEffect(() => {
-        const fetchWikipedia = async () => {
-            const wikipedia = await getWikipedia({ search: holiday.nombre });
-            console.log(wikipedia, 'wikicard', holiday.nombre)
-            setWikipedia(wikipedia);
-        }
-        fetchWikipedia();
-    }, [holiday.nombre]);
+export const Card = ({ holiday }: CardParams) => {
+  const [wikipedia, setWikipedia] = useState(null);
+  useEffect(() => {
+    const fetchWikipedia = async () => {
+      const wikipedia = await getWikipedia({ search: holiday.nombre });
+      console.log(wikipedia, "wikicard", holiday.nombre);
+      setWikipedia(wikipedia);
+    };
+    fetchWikipedia();
+  }, [holiday.nombre]);
 
-    console.log(wikipedia, 'wikipedia', holiday.nombre)
+  console.log(wikipedia, "wikipedia", holiday.nombre);
   return (
-    <div className="card">
-      <p>{holiday.fecha} - {holiday.nombre}</p>
+    <div
+      style={{
+        border: "1px solid black",
+        padding: "16px",
+        margin: "16px",
+        borderRadius: "8px",
+      }}
+    >
+      <p>
+        {holiday.fecha} - {holiday.nombre}
+      </p>
       {wikipedia && wikipedia[3] && wikipedia[3][0] ? (
-        <a 
-          href={wikipedia[3][0]} 
-          target="_blank" 
-          rel="noopener noreferrer" 
-        >
+        <a href={wikipedia[3][0]} target="_blank" rel="noopener noreferrer">
           Leer más en Wikipedia
         </a>
       ) : (
         <p>No se encontró información en Wikipedia</p>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default Card;
